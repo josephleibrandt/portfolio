bbc42183896cd22c54ce3f9a45ad306db9aaa31b
sinebe <enis.becirbegovic@gmail.com>
2017-06-13 14:30:46 -0600

    Delete PasswordControlInterface.java

--- PasswordControlInterface.java
+++
@@ -1,7 +0,0 @@
-package interfacepackage;
-import mainpackage.*;
-
-public interface PasswordControlInterface 
-{
-   public boolean ValidateAccess(String studentId, String username, String password );
-}
